e706d33906f738d0e043ad51572de650142b994f
ahmed -k <ahmedkamal200427@gmail.com>
2026-04-24 08:00:41 +0300

    test piped/env var input and output

---
+++ data_reader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+INPUT_DATA=$(cat)
+
+echo "Data Received: $INPUT_DATA"
+
+echo "Done"
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::{io::stdin, process::Stdio};
 
-use tokio::process::Command;
+use tokio::{io::AsyncWriteExt, process::Command};
 use shell_words;
-use tokio::io::{self, AsyncBufReadExt};
+use tokio::io::{self, AsyncBufReadExt, AsyncReadExt};
+
 
 // #[tokio::main]
 // async fn main() {
@@ -19,6 +20,11 @@ use tokio::io::{self, AsyncBufReadExt};
 
 #[tokio::main]
 async fn main() {
+    // trial_2_using_piped().await;
+    trial_3_using_env_vars().await;
+}
+
+async fn trial_1(){
     let argv = &["bash"];
 
     println!("Executing: {}", shell_words::join(argv));
@@ -57,4 +63,53 @@ async fn main() {
     reader.read_line(&mut input).await.unwrap();
 
     println!("You typed: {:?}", input);
+}
+
+async fn trial_2_using_piped(){
+    println!("current dir: {:?}", std::env::current_dir().unwrap());
+    let mut child = Command::new("./data_reader.sh")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    println!("Child process spawned with PID: {}", child.id().unwrap());
+
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+
+    println!("Child process stdin and stdout pipes created");
+
+    // Write to the child's stdin
+    stdin.write("Hi from rust".as_bytes()).await.expect("Failed to write to stdin");
+    drop(stdin);
+
+    println!("Data written to child process stdin");
+
+    // Read from the child's stdout
+    let mut stdout = child.stdout.take().expect("Failed to open stdout");
+    let mut response_buffer = String::new();
+    
+    // Read the entire output into a string
+    stdout.read_to_string(&mut response_buffer).await.expect("Failed to read from stdout");
+
+    // 3. Wait for exit
+    let _ = child.wait().await.expect("Failed to wait for child process");
+
+    println!("Child process output: {}", response_buffer);
+}
+
+
+async fn trial_3_using_env_vars(){
+
+    let output = Command::new("./data_reader_env_var.sh")
+        .env("VAR_DRASI", "Hello from Rust env var")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute command");
+
+    let mut stdout = output.stdout.expect("Failed to capture stdout");
+    let mut response_buffer = vec![0u8; 1024];
+    let n = stdout.read(&mut response_buffer).await.expect("Failed to read from stdout");
+
+    println!("Child process output: {}", String::from_utf8_lossy(&response_buffer[..n]));
 }
